c285a9abc6c7ecad5dc9b44b45e13e9bd5d6d0e4
MatheusGaldinoo <matheus.galdino.souza@ccc.ufcg.edu.br>
2025-07-11 23:05:58 -0300

    test: add new tests to best_path

--- src/dijkstra.py
+++ src/dijkstra.py
@@ -1,6 +1,7 @@
-class dijkstra:
+class Dijkstra:
     def __init__(self):
         pass
 
-    def best_path(self, c1,c2):
+    def best_path(self, city1, city2):
+        # TODO: Implement the Dijkstra's algorithm to find the best path between two cities
         pass
