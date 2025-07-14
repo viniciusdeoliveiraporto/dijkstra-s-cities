c4c8f70c0fb4fe1633dbf8203042175b59ba914b
aelsonjrdiniz <aelson.junior.araujo.diniz.cunha@ccc.ufcg.edu.br>
2025-07-14 20:38:16 -0300

    Adicionando a função de leitura de csv

--- src/dijkstra.py
+++ src/dijkstra.py
@@ -1,4 +1,6 @@
 from queue import PriorityQueue
+import os
+import csv
 
 class Dijkstra:
     def __init__(self):
@@ -61,7 +63,6 @@ class Dijkstra:
         distance_city1_to_city2 = distances[city2]
         path = [city2]
 
-        print(distance_city1_to_city2, "  TESTE ")
         if (distances[city2] != float('inf')): # Existe um caminho de city1 até city 2
             current_city = city2
             while(current_city != city1):
@@ -78,15 +79,41 @@ class Dijkstra:
 
         return distance_city1_to_city2, path
 
-
-
 d = Dijkstra()
+
+if __name__ == '__main__':
+    print('''Insira o caminho do csv contendo as informações das cidades
+             Cada linha do csv deve possuir o formato:
+                 nome da cidade referência, cidade vizinha 1, distância até cidade 1, cidade vizinha 2, distância até cidade 2, ....
+          ''')
+    csv_path = input()
+    grafo = dict()
+
+    if os.path.exists(csv_path):
+        with open(csv_path, "r", encoding="utf-8") as arquivo_csv:
+            cidade_origem = input("Qual o nome da cidade de origem? ").upper()
+            cidade_destino = input("Qual o nome da cidade de destino? ").upper()
+            leitor = csv.reader(arquivo_csv)
+            for linha in leitor:
+                if linha == []:
+                    break
+                dictionary = dict()
+                for i in range(1, len(linha), 2):
+                    distancia = linha[i+1].replace(",", ".")
+                    cidade = linha[i].upper()
+                    dictionary[cidade] = float(distancia)
+                cidade_ref = linha[0].upper()
+                grafo[cidade_ref] = dictionary
+            print(d.best_path(grafo, cidade_origem, cidade_destino))
+    else:
+        print("Arquivo não encontrado")
+
 
 #graph = {"CAMPINA GRANDE": {"LAGOA SECA": 2, "SÃO JOSÉ DA MATA": 11}, "LAGOA SECA": {"CAMPINA GRANDE": 2, "ESPERANÇA": 3}, "SÃO JOSÉ DA MATA": {"CAMPINA GRANDE": 11, "ESPERANÇA": 10}, "ESPERANÇA": {"SÃO JOSÉ DA MATA": 10, "LAGOA SECA": 3}, "SÃO PAULO": {"TESTE": 2}, "TESTE": {"SÃO PAULO": 2}}
 
-graph = {'CAMPINA GRANDE': {'LAGOA SECA': 8.8, 'QUEIMADAS': 17.0, 'Puxinanã': 14.6, 'Riachão do Bacamarte': 30.8, 'BOA VISTA': 44.1, 'SOLEDADE': 57.2, 'POCINHNHOS': 29.6, 'Fagundes': 26.9}}
+#graph = {'CAMPINA GRANDE': {'LAGOA SECA': 8.8, 'QUEIMADAS': 17.0, 'Puxinanã': 14.6, 'Riachão do Bacamarte': 30.8, 'BOA VISTA': 44.1, 'SOLEDADE': 57.2, 'POCINHNHOS': 29.6, 'Fagundes': 26.9}}
 
-print(d.best_path(graph, "CAMPINA GRANDE", "POCINHNHOS"))  #Esperado (5, ['CAMPINA GRANDE', 'LAGOA SECA', 'ESPERANÇA'])
+#print(d.best_path(graph, "CAMPINA GRANDE", "POCINHNHOS"))  #Esperado (5, ['CAMPINA GRANDE', 'LAGOA SECA', 'ESPERANÇA'])
 
 # print(d.best_path(graph, "Campina Grande", "Campina Grande")) #Esperado "(0.0,[])"  
 
