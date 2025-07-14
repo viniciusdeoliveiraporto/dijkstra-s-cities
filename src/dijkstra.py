957d478828cba91e6e5cc83addcafc9d6a8b6286
beatrizSM3 <beatriz.workspace@gmail.com>
2025-07-14 20:11:20 -0300

    corrigindo algumas coisas

--- src/dijkstra.py
+++ src/dijkstra.py
@@ -18,7 +18,21 @@ class Dijkstra:
         city2 = city2.upper() # padronizar para uppercase
 
 
-        if (graph.get(city1) is None or graph.get(city2) is None):
+       
+        distances = {}
+        previous = {}
+        cities = {}
+
+        for key1,value in graph.items():
+            cities[key1] = True
+            distances[key1] = float('inf')
+            previous[key1] = None
+            for key2 in value.keys():
+                cities[key2]= True
+                distances[key2] = float('inf')
+                previous[key2] = None
+
+        if (cities.get(city1) is None or cities.get(city2)  is None):
             return "No able path!"
         
         if (city1 == city2):
@@ -27,29 +41,27 @@ class Dijkstra:
         pq = PriorityQueue()
 
         pq.put((0.0, city1))
-
-        distances = {city: float('inf') for city in graph}
-        distances[city1] = 0.0
-        previous = {city: None for city in graph}
-
         
+        distances[city1] = 0.0
         while(not pq.empty()):
             current_distance, current_city = pq.get()
 
             if (distances[current_city] < current_distance):
                 continue
 
-            for neighbor in graph[current_city]:
-                new_distance = current_distance + graph[current_city][neighbor]
+            if (graph.get(current_city) is not None):
+                for neighbor in graph[current_city]:
+                    new_distance = current_distance + graph[current_city][neighbor]
 
-                if (distances[neighbor] > new_distance):
-                    distances[neighbor] = new_distance
-                    previous[neighbor] = current_city
-                    pq.put((new_distance, neighbor))
+                    if (distances[neighbor] > new_distance):
+                        distances[neighbor] = new_distance
+                        previous[neighbor] = current_city
+                        pq.put((new_distance, neighbor))
 
         distance_city1_to_city2 = distances[city2]
         path = [city2]
 
+        print(distance_city1_to_city2, "  TESTE ")
         if (distances[city2] != float('inf')): # Existe um caminho de city1 até city 2
             current_city = city2
             while(current_city != city1):
@@ -70,17 +82,19 @@ class Dijkstra:
 
 d = Dijkstra()
 
-graph = {"CAMPINA GRANDE": {"LAGOA SECA": 2, "SÃO JOSÉ DA MATA": 11}, "LAGOA SECA": {"CAMPINA GRANDE": 2, "ESPERANÇA": 3}, "SÃO JOSÉ DA MATA": {"CAMPINA GRANDE": 11, "ESPERANÇA": 10}, "ESPERANÇA": {"SÃO JOSÉ DA MATA": 10, "LAGOA SECA": 3}, "SÃO PAULO": {"TESTE": 2}, "TESTE": {"SÃO PAULO": 2}}
+#graph = {"CAMPINA GRANDE": {"LAGOA SECA": 2, "SÃO JOSÉ DA MATA": 11}, "LAGOA SECA": {"CAMPINA GRANDE": 2, "ESPERANÇA": 3}, "SÃO JOSÉ DA MATA": {"CAMPINA GRANDE": 11, "ESPERANÇA": 10}, "ESPERANÇA": {"SÃO JOSÉ DA MATA": 10, "LAGOA SECA": 3}, "SÃO PAULO": {"TESTE": 2}, "TESTE": {"SÃO PAULO": 2}}
 
-print(d.best_path(graph, "       Campina           Grande ", "ESPERANÇA"))  #Esperado (5, ['CAMPINA GRANDE', 'LAGOA SECA', 'ESPERANÇA'])
+graph = {'CAMPINA GRANDE': {'LAGOA SECA': 8.8, 'QUEIMADAS': 17.0, 'Puxinanã': 14.6, 'Riachão do Bacamarte': 30.8, 'BOA VISTA': 44.1, 'SOLEDADE': 57.2, 'POCINHNHOS': 29.6, 'Fagundes': 26.9}}
 
-print(d.best_path(graph, "Campina Grande", "Campina Grande")) #Esperado "(0.0,[])"  
+print(d.best_path(graph, "CAMPINA GRANDE", "POCINHNHOS"))  #Esperado (5, ['CAMPINA GRANDE', 'LAGOA SECA', 'ESPERANÇA'])
 
-print(d.best_path(graph, "Campina Grande", "João Pessoa")) #Esperado "No able path!"
+# print(d.best_path(graph, "Campina Grande", "Campina Grande")) #Esperado "(0.0,[])"  
+
+# print(d.best_path(graph, "Campina Grande", "João Pessoa")) #Esperado "No able path!"
 
 
-print(d.best_path(graph, "Campina Grande", "São Paulo")) #Esperado "No able path!" 
+# print(d.best_path(graph, "Campina Grande", "São Paulo")) #Esperado "No able path!" 
 
-print(d.best_path(graph, 2, "João Pessoa"))  # Esperado None
+# print(d.best_path(graph, 2, "João Pessoa"))  # Esperado None
 
 
